df5f289befca4c4c710bd0896cd4deba3dd98569
aspiering <aspiering@gmail.com>
2017-05-08 20:18:22 -0700

    fixing server URL

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ module.exports = {
 	appSecret: "990f9564dfeb441447eae22f27e883ab",
 	pageAccessToken: "EAANVx2wKV9kBAOdPKGEVMtZBDPF0zMRlJNBzgIxOaOsZCaYVplwn3vaBN0LFkZAsUsaV1x2me2B4E3ZB5ix3PbozQJFoX1PwnOU00hm8tG61EcAadPq4RfIYTN8CYYV6XXwuJjIMYQlU3BQEZBgAjBV1Kj5aCu4iK3DgV50ra2gZDZD",
 	validationToken: "my_voice_is_my_password_verify_me",
-	serverURL: "https://terribledamage.com:5000/webhook/",
+	serverURL: "https://terribledamage.com/webhook/",
 	privateKey: "certs/privkey.pem",
 	certificate: "certs/fullchain.pem"
 };
